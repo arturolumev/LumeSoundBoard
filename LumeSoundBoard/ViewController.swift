069328e76bb5e44edf2be1a368151d384ee91d64
Edwin Arturo Lume Ventura <edwin.lume@tecsup.edu.pe>
2023-05-24 09:00:43 -0500

    Initial Commit

---
+++ LumeSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LumeSoundBoard
+//
+//  Created by Arturo Lume on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
